f6256ba28daac2881610701f0ef7d363992a8085
lytex <julianlpc15@gmail.com>
2020-06-07 19:58:27 +0200

    code to plot wigner function

--- quantum.py
+++ quantum.py
@@ -39,12 +39,38 @@ solution = mesolve(H, ρ0, t, c_ops=measure)
 t = np.sqrt(1 + Λ) * t / np.pi
 zv, tv = np.meshgrid(z, t)
 
-scale = {"vmin": 0, "vmax": 0.2, "levels": 999}
+if False:
+    scale = {"vmin": 0, "vmax": 0.2, "levels": 999}
+    fig, ax = plt.subplots(1, 1, figsize=(7, 6))
+    cf = ax.contourf(zv, tv, ρ_zz, cmap=plt.get_cmap("plasma"), antialiased=False,
+        levels=np.linspace(0, 0.2, 1000))
+    ax.axis((-1.25, 1.25, t.min(), t.max()))
+    fig.colorbar(cf, ax=ax, ticks=[0, 0.05, 0.1, 0.15, 0.2], extend="both")
+    plt.draw()
+    plt.show()
+
+ϕ = np.linspace(-np.pi, np.pi, len(z))
+W = np.array([wigner(x, z, ϕ) for x in ρ])
+
 fig, ax = plt.subplots(1, 1, figsize=(7, 6))
-cf = ax.contourf(zv, tv, ρ_zz, cmap=plt.get_cmap("plasma"), antialiased=False,
-    levels=np.linspace(0, 0.2, 1000))
-ax.axis((-1.25, 1.25, t.min(), t.max()))
-fig.colorbar(cf, ax=ax, ticks=[0, 0.05, 0.1, 0.15, 0.2], extend="both")
+wmap = wigner_cmap(W)
+Wmax = max(W.max(), -W.min())
+nrm = mpl.colors.Normalize(-Wmax, Wmax)
+cf = ax.contourf(z, ϕ, W[0], 1000, cmap=mpl.cm.RdBu, norm=nrm)
+fig.colorbar(cf, ax=ax)
+def animate(i):
+    cf.ax.cla()
+    ax.contourf(z, ϕ, W[i], 1000, cmap=mpl.cm.RdBu, norm=nrm)
+    cf.set_array(W[i].flatten())
+
+Writer = writers['ffmpeg_file']
+video_duration_seconds = 20
+fps = len(t)/video_duration_seconds
+fps = fps if fps > 25.0 else 25.0 # Minumum of 25fps
+writer = Writer(fps=fps)
+video_file = 'W.mp4' 
+anim = FuncAnimation(fig, animate, frames=len(t) - 1, repeat=False, cache_frame_data=False)
+anim.save(video_file, writer=writer)
+
 plt.draw()
 plt.show()
-
